5127cf07f24b4c609591d0284011af4a08cd9d00
gabrielbmmaia <gbmmaia.contato@gmail.com>
2022-11-16 18:01:48 -0300

    feat (app): Criação da classe app para injeção do Hilt

--- app/src/main/java/com/example/filmix/core/Constants.kt
+++ app/src/main/java/com/example/filmix/core/Constants.kt
@@ -1,6 +1,10 @@
 package com.example.filmix.core
 
+import com.example.filmix.BuildConfig
+
 object Constants {
+
+    const val API_KEY = BuildConfig.API_KEY
 
     const val FILM_TABLE = "film_table"
 
---
+++ app/src/main/java/com/example/filmix/data/remote/FilmService.kt
@@ -0,0 +1,17 @@
+package com.example.filmix.data.remote
+
+import com.example.filmix.core.Constants
+import com.example.filmix.core.Constants.API_KEY
+import com.example.filmix.data.remote.model.FilmResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface FilmService {
+
+    @GET("movie/popular")
+    suspend fun getPopularFilms(
+        @Query("api_key") key: String = API_KEY,
+        @Query("page") page:Int
+    ): FilmResponse
+
+}
