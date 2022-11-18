4cfe1d1e33fe6438023bfb5a6e847ad321c11c88
gabrielbmmaia <gbmmaia.contato@gmail.com>
2022-11-18 13:48:38 -0300

    fix (gradle): Adicionado buildFeatures {dataBinding}

--- app/src/main/java/com/example/filmix/core/Constants.kt
+++ app/src/main/java/com/example/filmix/core/Constants.kt
@@ -4,15 +4,17 @@ import com.example.filmix.BuildConfig
 
 object Constants {
 
+    // Api Constants
     const val API_KEY = BuildConfig.API_KEY
-
     const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val BASE_POSTER_IMAGE_URL = "https://image.tmdb.org/t/p/w342"
 
+    // Room Constants
     const val FILM_TABLE = "film_table"
     const val FILM_REMOTE_KEYS_TABLE = "film_remote_keys_table"
-
     const val FILM_DATABASE = "film_database"
 
+    // Paging Constants
     const val FILM_PAGE_SIZE = 20
 
 }
