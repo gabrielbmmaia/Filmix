f560666d611ae3ae85a7bc47254c24951da08ec6
gabrielbmmaia <gbmmaia.contato@gmail.com>
2023-02-20 18:53:17 -0300

    fix (film_data): Criação de UseCases para list de filmes "Em breve" e "Em Cartaz"

--- app/src/main/java/com/example/filmix/features/shared/domain/useCases/FilmUseCases.kt
+++ app/src/main/java/com/example/filmix/features/shared/domain/useCases/FilmUseCases.kt
@@ -1,11 +1,11 @@
 package com.example.filmix.features.shared.domain.useCases
 
-import com.example.filmix.features.filmList.domain.useCases.GetFilmDetailsUseCase
-import com.example.filmix.features.filmList.domain.useCases.GetPopularFilmListUseCase
-import com.example.filmix.features.filmList.domain.useCases.GetTopRatedFilmsUseCase
+import com.example.filmix.features.filmList.domain.useCases.*
 
 data class FilmUseCases(
     val getPopularFilmList: GetPopularFilmListUseCase,
     val getFilmDetails: GetFilmDetailsUseCase,
-    val getTopRatedFilms: GetTopRatedFilmsUseCase
+    val getTopRatedFilms: GetTopRatedFilmsUseCase,
+    val getSoonFilmList: GetSoonFilmListUseCase,
+    val getTheatreFilmList: GetTheatreFilmListUseCase
 )
