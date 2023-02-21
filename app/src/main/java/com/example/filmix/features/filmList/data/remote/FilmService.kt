d435d1af05ee04ca4a8292220a95015074e8ab02
gabrielbmmaia <gbmmaia.contato@gmail.com>
2023-02-21 20:21:29 -0300

    fix (film_layout): Adicionado parametro "region" à chamada de lista de filmes "Populares" e "Bem avaliados"

--- app/src/main/java/com/example/filmix/features/filmList/data/remote/FilmService.kt
+++ app/src/main/java/com/example/filmix/features/filmList/data/remote/FilmService.kt
@@ -3,8 +3,6 @@ package com.example.filmix.features.filmList.data.remote
 import com.example.filmix.core.Constants.API_KEY
 import com.example.filmix.core.Constants.DEFAULT_LANGUAGE
 import com.example.filmix.core.Constants.DEFAULT_REGION
-import com.example.filmix.core.Constants.FILM_MEDIA_TYPE
-import com.example.filmix.core.Constants.WEEK_TIME_WINDOW
 import com.example.filmix.features.filmList.data.model.FilmDetailsDto
 import com.example.filmix.features.shared.data.model.MediaResponse
 import retrofit2.http.GET
@@ -13,20 +11,20 @@ import retrofit2.http.Query
 
 interface FilmService {
 
-    @GET("trending/{media_type}/{time_window}")
+    @GET("movie/popular")
     suspend fun getPopularFilms(
-        @Path("media_type") mediaType: String = FILM_MEDIA_TYPE,
-        @Path("time_window") timeWindow: String = WEEK_TIME_WINDOW,
         @Query("api_key") key: String = API_KEY,
         @Query("page") page: Int,
-        @Query("language") language: String = DEFAULT_LANGUAGE
+        @Query("language") language: String = DEFAULT_LANGUAGE,
+        @Query("region") region: String = DEFAULT_REGION
     ): MediaResponse
 
     @GET("movie/top_rated")
     suspend fun getTopRatedFilms(
         @Query("page") page: Int,
         @Query("api_key") key: String = API_KEY,
-        @Query("language") language: String = DEFAULT_LANGUAGE
+        @Query("language") language: String = DEFAULT_LANGUAGE,
+        @Query("region") region: String = DEFAULT_REGION
     ): MediaResponse
 
     @GET("movie/upcoming")
